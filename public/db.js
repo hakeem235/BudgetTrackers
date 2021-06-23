2f5e5331d3c69280191a6b9b72139beffca969e4
ahmed hakeem <hakeemkoo@hotmail.com>
2021-06-22 23:14:37 -0400

    creating object store called "pending"

--- public/db.js
+++ public/db.js
@@ -4,3 +4,8 @@ let db;
 // creating a request for budgets database
 const request = indexedDB.open("budgets", 1);
 
+request.onupgradeneeded = (event) => {
+    // creating object store called "pending" and setting autoIncrement to true
+    const db = event.target.result;
+    db.createObjectStore("pending", { autoIncrement: true });
+  };
